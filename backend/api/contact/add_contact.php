1a11aab62c9c7ae524b46be145930ba1105717d5
Joyed <jabirjoyed99@gmail.com>
2025-03-11 19:19:41 +0600

    joyed

--- backend/api/contact/add_contact.php
+++ backend/api/contact/add_contact.php
@@ -5,8 +5,8 @@
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Allow-Headers: Content-Type");
 
-    //  require_once __DIR__ . '/../../classes/crm--for-contact.php';
-     require_once __DIR__ . '/../../classes/crm.php';
+     require_once __DIR__ . '/../../classes/crm--for-contact.php';
+    //  require_once __DIR__ . '/../../classes/crm.php';
 
    
     if($_SERVER['REQUEST_METHOD']==='POST'){
